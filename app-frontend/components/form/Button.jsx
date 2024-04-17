cc25eec0eac479a772774813d7882047890386e1
Maria Pogosjan <maria@bazooka.se>
2024-04-17 12:10:45 +0200

    feat: added step method to form, both forward and backward

--- app-frontend/components/form/Button.jsx
+++ app-frontend/components/form/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Button = ({ type, buttonLabel }) => {
+const Button = ({ type, buttonLabel,  onBack, classes, disabled }) => {
     return (
-        <button type={type} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button 
+            type={type} 
+            onClick={onBack}
+            disabled={disabled}
+            className={`${classes} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4`}
+        >
             {buttonLabel}
         </button>
     );
--- app-frontend/components/form/Form.jsx
+++ app-frontend/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 import Container from '../Container';
@@ -9,37 +9,66 @@ import Button from './Button';
 import { fields } from '../../reusable/fields';
 
 const Form = () => {
+    const [formStep, setFormStep] = useState(1);
+    const totalForms = 4;
+
     const { register, handleSubmit, formState: { errors } } = useForm({
         mode: 'onBlur',
     });
 
     const onSubmit =  (data) => {
-       console.log(data);
-      };
+        if (formStep !== totalForms) {
+            setFormStep(formStep + 1);
+        }
+    };
+
+    const onBack = () => {
+        if (formStep > 1) {
+            setFormStep(formStep - 1);
+        }
+    };
+
+    const FieldRenderer = ({ formStep }) => (
+        <>
+            {fields.filter((field) => field.step === formStep).map((field) => (
+                <Container key={field.id}  classes={'flex space-x-4 pt-4'}>
+                    <Label htmlFor={field.id} labelName={field.labelName} />
+                    <Container classes={'flex flex-col w-full'}>
+                        <Input
+                            id={field.id}
+                            type={field.type}
+                            name={field.name}
+                            register={register}
+                            validation={field.validation}
+                        />
+                        {errors[field.name] && <p className="text-red-500 pt-2 text-left text-xs">{errors[field.name].message}</p>}
+                    </Container>
+                </Container>
+            ))}
+        </>
+    );
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='w-1/2 p-6 border border-gray-300'>
-            <>
-                {fields.map((field) => (
-                    <Container key={field.id}  classes={'flex space-x-4 pt-4'}>
-                        <Label htmlFor={field.id} labelName={field.labelName} />
-                        <Container classes={'flex flex-col w-full'}>
-                            <Input
-                                id={field.id}
-                                type={field.type}
-                                name={field.name}
-                                register={register}
-                                validation={field.validation}
-                            />
-                            {errors[field.name] && <p className="text-red-500 pt-2 text-left text-xs">{errors[field.name].message}</p>}
-                        </Container>
-                    </Container>
-                ))}
-            </>
+            {formStep === 1 && <FieldRenderer formStep={1} />}
+            {formStep === 2 && <FieldRenderer formStep={2} />}
+            {formStep === 3 && <FieldRenderer formStep={3} />}
             <Container classes="flex justify-center space-x-4 mt-6">
-                <Button type="button" buttonLabel="Back"/>
-                <Button type="submit" buttonLabel="Next"/>
+                <Button 
+                    type="button" 
+                    buttonLabel="Back" 
+                    onBack={onBack}
+                    classes={formStep === 1 ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}
+                />
+                {formStep !== 4 &&   
+                    <Button 
+                        type="submit" buttonLabel="Next" 
+                        disabled={formStep === totalForms}  
+                        classes={formStep === totalForms ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''} 
+                    />
+                }
             </Container>
+            {formStep === 4 && <div><h2>Thank you for your order!</h2></div>}
         </form>
     );
 };
